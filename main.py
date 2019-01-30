3ea238fcf6cea6049dfaa0346aa9e2d0679b914e
Soumya Mukhija <39940437+SoumyaMukhija@users.noreply.github.com>
2019-01-30 23:38:12 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,21 +1,38 @@
 from flask import Flask, render_template
 import os
+#from data import Articles
+from flask_pymongo import PyMongo
 
 
-pocket = Flask (__name__)
+wallet = Flask (__name__)
+wallet.config["MONGO_URI"] = "mongodb://localhost:27017/userdata"
 
-@pocket.route('/')
+mongo=PyMongo(wallet)
+#Articles=Articles()
+#mongo.db.createCollection('userdata')
+
+
+@wallet.route('/')
 def home():
 	return render_template('home.html')
 
-@pocket.route('/about')
-def about():
-	return render_template('about.html')
-
-@pocket.route('/contact')
+@wallet.route('/contact')
 def contact():
 	return render_template('contact.html')
 
+@wallet.route('/articles')
+def articles():
+	return render_template('articles.html', articles=Articles)
+
+@wallet.route('/enter')
+def enter():
+	user= mongo.db.userdata
+	user.insert_one({'name' : 'Soumya'})
+	return 'Added User!'
+
+@wallet.route('/welcome')
+def welcome(): 
+	return render_template('welcomepage.html')
 
 if __name__== ('__main__'):
-	pocket.run(debug=True)
+	wallet.run(debug=True)
